62b7f93a2dc881ee5397290feed7651a8200ac3b
Ronny Alvarado <rsalvarado777@gmail.com>
2019-11-17 23:50:42 -0600

    Boiler plating for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// module imports
+import React from "react";
+import styled from "styled-components";
+import { Route, Link } from "react-router-dom";
+
+// file imports
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer></AppContainer>;
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  box-sizing: border-box;
+  height: 98vh;
+  background: linear-gradient(#087475, #152b40);
+`;
