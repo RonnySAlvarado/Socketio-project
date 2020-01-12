d5a643ff8685f3cc73da41476e6e9e94e9c5a91b
Ronny Alvarado <rsalvarado777@gmail.com>
2020-01-11 21:11:35 -0600

    Updated the comments for Landing.test.js to better explain what's going on in this file

--- client/src/views/Landing/Landing.test.js
+++ client/src/views/Landing/Landing.test.js
@@ -5,14 +5,19 @@ import { Router } from "react-router-dom";
 
 import Landing from "./Landing";
 
-test("Some comments here", () => {
+test("Changes the history to include the /dashboard route", () => {
+  // creates an instance of the history object to pass into the Router component as prop
   const history = createMemoryHistory();
+  // Builds the virtual DOM tree and stores it in landingPage so it'll have testing-library methods
   const landingPage = render(
     <Router history={history}>
       <Landing />
     </Router>
   );
+  // Grab the anchor node (Link component)
   const button = landingPage.getByText(/enter here/i);
+  // Clicks the node
   fireEvent.click(button);
+  // Expects an update to the path name to be /dashboard
   expect(history.location.pathname).toBe("/dashboard");
 });
